0ebf5ba881e741327905e462d3be0e9f3597c112
pak2601 <137136935+pak2601@users.noreply.github.com>
2025-05-27 12:43:18 +1000

    Create Key_Schedule.py

---
+++ Key_Schedule.py
@@ -0,0 +1,25 @@
+#shift left:
+def shift_left(k, pos): 
+    return k[pos:] + k[:pos] #move the first n bit to the end
+
+#apply pc1
+def apply_pc1(s):
+    result = ""
+    
+    for i in range(len(PC1)):
+        result = result + s[PC1[i] -1]
+
+    return result
+
+#split key 
+def split_key(s):
+    #split 56-bit key into two 28-bit key halves
+    return [s[:28], s[28:]]
+
+
+#apply pc2
+def apply_pc2(s):
+    result = ""
+    for i in range(len(PC2)):
+        result = result + s[PC2[i]-1]
+    return result
