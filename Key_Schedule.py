8f9bf0f55f7ce5bcd3403d1c349c0ec27834e184
sippbing <trungtuyenng@gmail.com>
2025-05-28 23:23:58 +1000

    Add safeguards to Key_Schedule.py methods

--- Key_Schedule.py
+++ Key_Schedule.py
@@ -1,11 +1,15 @@
+from DES_Components import PC1, PC2
+
 #shift left:
 def shift_left(k, pos): 
     return k[pos:] + k[:pos] #move the first n bit to the end
 
 #apply pc1
 def apply_pc1(s):
+    if len(s) != 64:
+        raise ValueError("Master Key must be 64-bit to perform PC1")
+
     result = ""
-    
     for i in range(len(PC1)):
         result = result + s[PC1[i] -1]
 
@@ -19,6 +23,9 @@ def split_key(s):
 
 #apply pc2
 def apply_pc2(s):
+    if len(s) != 48:
+        raise ValueError("Round Key must be 48-bit to perform PC2")
+
     result = ""
     for i in range(len(PC2)):
         result = result + s[PC2[i]-1]
