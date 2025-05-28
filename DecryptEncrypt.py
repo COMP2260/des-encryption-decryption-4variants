c3ea808318a1fc4423a9346baf946a32b2e782f8
sippbing <trungtuyenng@gmail.com>
2025-05-29 01:15:09 +1000

    Add generate_round_keys() for Key Generation

--- DecryptEncrypt.py
+++ DecryptEncrypt.py
@@ -1,8 +1,11 @@
+from DES_IMPLEMENTATION import *
+from F_FUNCTION import f_function
+from Key_Schedule import *
+
 def encrypt(plaintext, key, version = 'DES0'):
     if len(plaintext) != 64:
         raise ValueError("Plaintext must be 64 bits")
 
-    
     #get round keys
     round_keys = generate_round_keys(key)
 
@@ -61,3 +64,32 @@ def decrypt(ciphertext, key, version= 'DES0'):
     plaintext = inverse_perm(combine)
         #return ciphertext
     return plaintext
+
+def generate_round_keys(master):
+    # apply PC1 to master key
+    permuted = apply_pc1(master)
+
+    # split permuted master key into 2 halves
+    C, D, splits = ["0"] * 16, ["0"] * 16, split_key(permuted)
+    C[0] = splits[0]
+    D[0] = splits[1]
+
+    round_keys = list()
+    for i in range(16):
+        # Perform left shift on each half
+        # The number of bits to shift for each round is predefined in Rotation
+        C[i] = shift_left(C[i], Rotation[i])
+        D[i] = shift_left(D[i], Rotation[i])
+
+        # Join and apply PC2 transformation to the halves
+        joined = C[i] + D[i]
+        round_keys.append(apply_pc2(joined))
+
+        # Prepare for next round key generation
+        if (i != 15):
+            C[i + 1] = C[i]
+            D[i + 1] = D[i]
+    
+    return round_keys
+
+    
