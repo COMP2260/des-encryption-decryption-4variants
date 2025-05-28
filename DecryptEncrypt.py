cf5a670861263041d6e397b2a7723697a2363280
pak2601 <137136935+pak2601@users.noreply.github.com>
2025-05-28 23:12:39 +1000

    Update DecryptEncrypt.py

--- DecryptEncrypt.py
+++ DecryptEncrypt.py
@@ -31,3 +31,33 @@ def encrypt(plaintext, key, version = 'DES0'):
         #return ciphertext
     return ciphertext
             
+
+def decrypt(ciphertext, key, version= 'DES0'):
+    if len(ciphertext) != 64:
+        raise ValueError("Ciphertext must be 64-bit long")
+    #reverse the list for decryption
+    round_keys = generate_round_keys(key)[::-1]
+    
+    #initial permutaiton
+    permuted = initial_perm(ciphertext)
+
+    #the rest will be implmented same with the encryption function
+    L = permuted[:32]
+    R = permuted[32:]
+
+    for i in range(16):
+        #swapping
+        old_R = R
+        #apply left function to right half
+        f_func_R = f_function(R, round_keys[i], version)
+
+        #xor with left half
+        R = xor(L, f_func_R)
+        #swap back with original R
+        L = old_R
+
+    #combine the final result for inverse permutaiton
+    combine = R+L
+    plaintext = inverse_perm(combine)
+        #return ciphertext
+    return plaintext
